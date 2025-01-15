e9a2e597f34853a01439221a07fe74f5280c15bf
IriMerkulova <151544565+IriMerkulova@users.noreply.github.com>
2025-01-15 07:58:11 +0300

    Update module_5_3.py
    Добавление проверки принадлежности к классам

--- module_5_3.py
+++ module_5_3.py
@@ -1,8 +1,9 @@
 # Задача "Нужно больше этажей":
 class House:
-    def __init__(self, name, number_of_floors):
+        def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
+
     def go_to(self, new_floor):
         if new_floor > self.number_of_floors or new_floor < 1:
             print('Такого этажа не существует')
@@ -10,34 +11,93 @@ class House:
             for floor in range(1, new_floor + 1):
                 print(floor)
 
-    def isinstance(self, other):
-        return isinstance(other, int)
+
     def __len__(self):
         return self.number_of_floors
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
-    def __eq__(self, x):
-        return self.number_of_floors == x.number_of_floors
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
+        else:
+            return False
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
+        else:
+            return NotImplemented
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors <= other
+        else:
+            return NotImplemented
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors > other
+        else:
+            return NotImplemented
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors >= other
+        else:
+            return NotImplemented
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors != other
+        else:
+            return True
+
     def __add__(self, value):
-        return House(self.name, self.number_of_floors + value)
+        if isinstance(value, House):
+            self.number_of_floors = self.number_of_floors + value.number_of_floors
+            return self
+        elif isinstance(value, int):
+            self.number_of_floors = self.number_of_floors + value
+            return self
+        else:
+            NotImplemented
     def __radd__(self, value):
         return House(self.name, self.number_of_floors + value)
     def __iadd__(self, value):
-        return House(self.name, self.number_of_floors + value)
+        if isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
+            return self
+        elif isinstance(value, int):
+            self.number_of_floors += value
+            return self
+        else:
+            NotImplemented
     def __sub__(self, value):
-        return House(self.name, self.number_of_floors - value)
+        if isinstance(value, House):
+            self.number_of_floors -= value.number_of_floors
+            return self
+        elif isinstance(value, int):
+            self.number_of_floors -= value
+            return self
+        else:
+            NotImplemented
     def __isub__(self, value):
-        return House(self.name, self.number_of_floors - value)
+        if isinstance(value, House):
+            self.number_of_floors -= value.number_of_floors
+            return self
+        elif isinstance(value, int):
+            self.number_of_floors -= value
+            return self
+        else:
+            NotImplemented
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
